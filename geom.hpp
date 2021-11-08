70340d1d844818c9027bb84973c294a909672cd7
Alexandra Stancheva <66909315+allks12@users.noreply.github.com>
2021-11-08 09:22:47 +0200

    Update geom.hpp

--- geom.hpp
+++ geom.hpp
@@ -34,7 +34,9 @@ struct Line {
     Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {}
 
     Line(const Point& p1, const Point& p2) {
-        A = 0; B = 0; C = 0;
+        A = p2.y - p1.y;
+        B = p1.x - p2.x;
+        C = -(A*p1.x + B*p1.y);
     }
 
     bool parallel(const Line& other) const {
@@ -42,7 +44,7 @@ struct Line {
     }
 
     Line parallel(const Point& p) {
-        return Line(0,0,0);
+        return Line(A,B,-(A*p.x + B*p.y));
     }
 
     bool perpendicular(const Line& other) const {
